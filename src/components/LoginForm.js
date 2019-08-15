101d20a1980f15471d24446247851ee3b3458884
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 19:00:52 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,10 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
 
   render() {
